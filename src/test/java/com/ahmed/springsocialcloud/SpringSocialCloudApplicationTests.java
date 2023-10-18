6dfb806ce80832e1a2c6946eb62ed35bfb901f11
Haykal <44018986+AhmedARmohamed@users.noreply.github.com>
2023-10-18 23:01:53 +0300

    Add home controller

---
+++ src/test/java/com/ahmed/springsocialcloud/SpringSocialCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmed.springsocialcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocialCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
